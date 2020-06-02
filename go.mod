bf9db40f92f47d88e15e914ad1f0441062b627ec
胡江涛 <jt_hu@trip.com>
2020-06-02 16:44:54 +0800

    details: 测试接口上传

---
+++ go.mod
@@ -0,0 +1,13 @@
+module blog-for-go
+
+go 1.14
+
+require (
+	github.com/Joker/jade v1.0.0 // indirect
+	github.com/flosch/pongo2 v0.0.0-20200518135938-dfb43dbdc22a // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/iris-contrib/formBinder v5.0.0+incompatible // indirect
+	github.com/kataras/iris v11.1.1+incompatible
+	github.com/kataras/iris/v12 v12.1.8
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+)
---
+++ test/web_demo.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/mvc"
+	"strconv"
+)
+
+func main() {
+	app := iris.New()
+
+	tmpl := iris.HTML("./test/templates", ".html")
+	tmpl.Reload(true) // reload templates on each request (development mode)
+	app.RegisterView(tmpl)
+
+	app.OnErrorCode(iris.StatusNotFound, func(ctx iris.Context) {
+		ctx.View("404.html")
+	})
+
+	app.Get("/", func(ctx iris.Context){
+		ctx.ViewData("Title", "Index Page Of Go(Iris)")
+		ctx.ViewData("Msg", "这是一条测试消息！")
+		ctx.View("index.html")
+	})
+	app.Get("/greeting/{userName}", func(ctx iris.Context) {
+		ctx.Writef("hello, %s", ctx.Params().Get("userName"))
+	})
+
+	// 注册函数
+	app.Macros().Get("int").RegisterFunc("min", func(minVal int) func(string) bool{
+		return func(paramVal string) bool {
+			n, err := strconv.Atoi(paramVal)
+
+			if err != nil {
+				return false
+			}
+
+			return n >= minVal
+		}
+	})
+	app.Get("/testMin/{id:int min(1) else 504}", func(ctx iris.Context) {
+		id, _ := ctx.Params().GetInt("id")
+		ctx.Writef("Hello id: %d", id)
+	})
+
+	app.Get("/game/{name:alphabetical}/level/{level:int}", func(ctx iris.Context) {
+		ctx.Writef("name: %s | level: %s", ctx.Params().Get("name"), ctx.Params().Get("level"))
+	})
+
+	//app.Get("/single_file/{myfile:file}", func(ctx iris.Context) {
+	//	ctx.Writef("file type validates if the parameter value has a form of a file name, got: %s", ctx.Params().Get("myfile"))
+	//})
+
+	app.Get("/myfiles/{directory:path}", func(ctx iris.Context) {
+		ctx.Writef("path type accepts any number of path segments, path after /myfiles/ is: %s", ctx.Params().Get("directory"))
+	})
+
+	// controller 演示
+	mvc.New(app.Party("/hello")).Handle(new(ExpController))
+
+
+	app.Run(iris.Addr(":8080"))
+}
+
+type ExpController struct {
+
+}
+
+
+type ResponseData struct {
+	Code int 	`json:"code"`
+	Msg string 	`json:"msg"`
+	Data interface{} `json: "data"`
+}
+
+func GetSuccessData(data interface{}) *ResponseData{
+	return &ResponseData{200, "hello", data}
+}
+
+func (c *ExpController) Get() mvc.Result{
+	return mvc.View{
+		Name: "index.html",
+		Data: map[string]interface{} {
+			"Title": "hello",
+			"Msg": "message",
+		},
+	}
+}
+
+func (c *ExpController) GetJson() interface{} {
+	return GetSuccessData("这是一条数据")
+}
